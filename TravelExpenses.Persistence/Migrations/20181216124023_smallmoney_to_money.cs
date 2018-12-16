5647df642640ce755b8655b4acefbf27d4ba4de2
Erik Sharp <mr.erik.sharp@gmail.com>
2018-12-16 12:47:58 +0000

    smallmoney to money

---
+++ TravelExpenses.Persistence/Migrations/20181216124023_smallmoney_to_money.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelExpenses.Persistence.Migrations
+{
+    public partial class smallmoney_to_money : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql =
+@"ALTER TABLE [app].[Transaction]
+ALTER COLUMN [Amount] money;";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql =
+@"ALTER TABLE [app].[Transaction]
+ALTER COLUMN [Amount] smallmoney;";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
