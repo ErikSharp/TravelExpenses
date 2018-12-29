cdc03f7f7f2384c8943deeab42e6e0f378491ac0
Erik Sharp <mr.erik.sharp@gmail.com>
2018-12-29 15:07:43 +0000

    Created new build with keyword fix

--- TravelExpenses.Client/src/components/setup/AddKeyword.vue
+++ TravelExpenses.Client/src/components/setup/AddKeyword.vue
@@ -83,7 +83,7 @@ export default {
       return errors
     },
     items() {
-      return this.$store.state.Keyword.countries
+      return this.$store.state.Keyword.keywords
     },
     busy() {
       return this.$store.state.Keyword.addKeywordBusy
