9bdd3f9d31f027cf97b998beeafcde908d3055ec
CaptainBedpan <mr.erik.sharp@gmail.com>
2019-02-18 11:17:39 +0000

    improvement

--- TravelExpenses.Persistence/Scripts/UsersAndTransCounts.sql
+++ TravelExpenses.Persistence/Scripts/UsersAndTransCounts.sql
@@ -1,5 +1,5 @@
 -- users and their transaction counts
-select u.Email, u.id as userId, count(*)
+select u.Email, u.id as UserId, count(*) as TransactionCount
 from app.Users u
 left join app.[Transaction] t on u.id = t.UserId
 group by u.Email, u.id
