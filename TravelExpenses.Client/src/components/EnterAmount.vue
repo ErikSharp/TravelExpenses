facf779152aeab246f837822c914ceaa56c734f2
CaptainBedpan <mr.erik.sharp@gmail.com>
2019-02-19 16:22:50 +0000

    Initial work with the reconcile cash calculator

---
+++ TravelExpenses.Client/src/components/EnterAmount.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-dialog v-model="dialog" max-width="290">
+    <v-btn slot="activator" flat class="primary my-3">Enter Amount</v-btn>
+    <v-card>
+      <v-card-title>
+        User the calculator below to enter the amount{{
+          currencyString ? ` of ${currencyString} on-hand` : ''
+        }}
+      </v-card-title>
+      <v-container grid-list-md class="pt-0">
+        <v-text-field
+          class="readout font-weight-bold"
+          readonly
+          solo
+          value="Erik"
+        ></v-text-field>
+        <v-layout row wrap>
+          <v-flex xs4 v-for="n in 12" :key="n">
+            <v-btn large round class="input px-0 font-weight-bold display-1">{{
+              getKeyString(n)
+            }}</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <v-card-actions>
+        <v-btn color="primary" @click="dialog = false">CLOSE</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    currencyString: String
+  },
+  data() {
+    return {
+      dialog: false
+    }
+  },
+  methods: {
+    getKeyString(index) {
+      switch (index) {
+        case 1:
+          return '7'
+        case 2:
+          return '8'
+        case 3:
+          return '9'
+        case 4:
+          return '4'
+        case 5:
+          return '5'
+        case 6:
+          return '6'
+        case 7:
+          return '1'
+        case 8:
+          return '2'
+        case 9:
+          return '3'
+        case 10:
+          return '+'
+        case 11:
+          return '0'
+        case 12:
+          return '.'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-btn,
+.input {
+  min-width: 60px;
+}
+</style>
--- TravelExpenses.Client/src/components/reconcile/ReconcileCashCalc.vue
+++ TravelExpenses.Client/src/components/reconcile/ReconcileCashCalc.vue
@@ -1,17 +1,125 @@
 <template>
   <div>
-    <h3>Cash Calculator</h3>
+    <h3 class="white--text">Cash calculator</h3>
+    <v-select
+      :items="currencies"
+      v-model="currency"
+      return-object
+      :error-messages="currencyErrors"
+      box
+      background-color="white"
+      color="primary"
+      label="Currency"
+      @input="$v.currency.$touch()"
+      @blur="$v.currency.$touch()"
+    >
+      <template slot="selection" slot-scope="data">
+        <div>
+          <span>
+            <strong>{{ data.item.isoCode }}</strong>
+            - {{ data.item.currencyName }}
+          </span>
+        </div>
+      </template>
+      <template slot="item" slot-scope="data">
+        <div>
+          <span>
+            <strong>{{ data.item.isoCode }}</strong>
+            - {{ data.item.currencyName }}
+          </span>
+        </div>
+      </template>
+    </v-select>
+    <v-select
+      :items="locations"
+      v-model="location"
+      return-object
+      :error-messages="locationErrors"
+      box
+      background-color="white"
+      color="primary"
+      label="Location"
+      @input="$v.location.$touch()"
+      @blur="$v.location.$touch()"
+    >
+      <template slot="selection" slot-scope="data">
+        {{ getLocationString(data.item) }}
+      </template>
+      <template slot="item" slot-scope="data">
+        {{ getLocationString(data.item) }}
+      </template>
+    </v-select>
+    <enter-amount currencyString="MXN" />
     <v-btn @click="navToSummary">Done</v-btn>
   </div>
 </template>
 
 <script>
 import Windows from '@/common/enums/ReconcileWindows.js'
+import EnterAmount from '@/components/EnterAmount.vue'
+import { required } from 'vuelidate/lib/validators'
+import sortBy from 'lodash/sortBy'
 
 export default {
+  components: {
+    EnterAmount
+  },
+  data() {
+    return {
+      currency: {},
+      location: {}
+    }
+  },
+  validations() {
+    const result = {
+      currency: {
+        required
+      },
+      location: {
+        required
+      }
+    }
+
+    return result
+  },
   methods: {
     navToSummary() {
       this.$store.dispatch('Reconcile/setReconcileWindowId', Windows.summary)
+    },
+    getLocationString(locationObj) {
+      const country = this.$store.getters['Country/findCountry'](
+        locationObj.countryId
+      )
+
+      if (country) {
+        return `${locationObj.locationName}, ${country.countryName}`
+      } else {
+        return locationObj.locationName
+      }
+    }
+  },
+  computed: {
+    currencyErrors() {
+      const errors = []
+
+      if (!this.$v.currency.$dirty) return errors
+
+      !this.$v.currency.required && errors.push('A currency is required')
+      return errors
+    },
+    locationErrors() {
+      const errors = []
+
+      if (!this.$v.location.$dirty) return errors
+
+      !this.$v.location.required && errors.push('A location is required')
+      return errors
+    },
+    currencies() {
+      return this.$store.state.Currency.currencies
+    },
+    locations() {
+      return sortBy(this.$store.state.Location.locations, l => l.locationName)
     }
   }
 }
