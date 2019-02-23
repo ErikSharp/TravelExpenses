df9522268887de8925ed385f30d6e8cfc734953b
CaptainBedpan <mr.erik.sharp@gmail.com>
2019-02-23 11:52:32 +0000

    Fixed query for when there are no transactions

--- TravelExpenses.Persistence/Scripts/UsersAndTransCounts.sql
+++ TravelExpenses.Persistence/Scripts/UsersAndTransCounts.sql
@@ -1,5 +1,5 @@
 -- users and their transaction counts
-select u.Email, u.id as UserId, count(*) as TransactionCount
+select u.Email, u.id as UserId, count(t.Id) as TransactionCount
 from app.Users u
 left join app.[Transaction] t on u.id = t.UserId
 group by u.Email, u.id
