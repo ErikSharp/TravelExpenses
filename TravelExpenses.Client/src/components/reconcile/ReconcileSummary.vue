73dd6a4198f805c3135c16babbe5b1c192591600
CaptainBedpan <mr.erik.sharp@gmail.com>
2019-02-27 12:35:12 +0000

    Using iso codes for space

--- TravelExpenses.Client/src/components/reconcile/ReconcileSummary.vue
+++ TravelExpenses.Client/src/components/reconcile/ReconcileSummary.vue
@@ -16,28 +16,28 @@
         <v-divider class="mb-3"></v-divider>
         <v-layout row>
           <v-flex grow>
-            <h3>{{ `${currency.currencyName} withdrawn:` }}</h3>
+            <h3>{{ `${currency.isoCode} withdrawn:` }}</h3>
           </v-flex>
           <v-flex shrink>
             <h3 class="text-xs-right">
               {{
-                reconcileSummary.totalWithdrawn
-                  ? formatNumber(reconcileSummary.totalWithdrawn)
-                  : ''
+              reconcileSummary.totalWithdrawn
+              ? formatNumber(reconcileSummary.totalWithdrawn)
+              : ''
               }}
             </h3>
           </v-flex>
         </v-layout>
         <v-layout row>
           <v-flex grow>
-            <h3>{{ `${currency.currencyName} spent:` }}</h3>
+            <h3>{{ `${currency.isoCode} spent:` }}</h3>
           </v-flex>
           <v-flex shrink>
             <h3>
               {{
-                reconcileSummary.totalSpent
-                  ? formatNumber(reconcileSummary.totalSpent)
-                  : ''
+              reconcileSummary.totalSpent
+              ? formatNumber(reconcileSummary.totalSpent)
+              : '0'
               }}
             </h3>
           </v-flex>
@@ -79,9 +79,7 @@
           <h3>You cash on-hand is correct.</h3>
           <v-layout row>
             <v-flex grow>
-              <h2 style="display: inline" class="green--text">
-                Reconciliation complete
-              </h2>
+              <h2 style="display: inline" class="green--text">Reconciliation complete</h2>
             </v-flex>
             <v-flex shrink>
               <v-icon color="green">done_outline</v-icon>
@@ -91,23 +89,17 @@
         <div v-else>
           <v-layout row align-center>
             <v-flex grow>
-              <h3 class="red--text mr-2" style="display: inline">
-                {{ resultString }}
-              </h3>
+              <h3 class="red--text mr-2" style="display: inline">{{ resultString }}</h3>
             </v-flex>
             <v-flex shrink>
-              <v-icon large color="red">
-                {{ haveNetGain ? 'trending_up' : 'trending_down' }}
-              </v-icon>
+              <v-icon large color="red">{{ haveNetGain ? 'trending_up' : 'trending_down' }}</v-icon>
             </v-flex>
           </v-layout>
         </div>
       </v-card-text>
     </v-card>
     <v-layout v-if="!numbersMatch" row justify-center>
-      <v-btn dark color="primary" class="mt-3" @click="navToInvestigation"
-        >Investigate</v-btn
-      >
+      <v-btn dark color="primary" class="mt-3" @click="navToInvestigation">Investigate</v-btn>
     </v-layout>
   </v-container>
 </template>
